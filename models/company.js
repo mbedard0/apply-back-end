45eee0e7515293001a74fae4a29565efba74aa31
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-20 15:00:22 -0500

    update company creation functionality

--- models/company.js
+++ models/company.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const companySchema = new mongoose.Schema({
   companyName: String,
+  description: String,
   size: {type: String, enum: ['1-10','11-50','51-200','201-1000','1001-5000','5001-10000','10001+']},
   rating: Number,
   location: String,
