6204a28b66794d2ea8dc1518c984270ee38104c2
Meiling Bedard <meiling.bedard@gmail.com>
2022-01-22 20:52:52 -0500

    set up function to check profile status for company or individual

--- routes/companies.js
+++ routes/companies.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import * as companiesCtrl from '../controllers/companies.js'
-import { checkAuth } from '../middleware/auth.js'
+import { checkAuth, checkCompanyAuth } from '../middleware/auth.js'
 
 const router = Router()
 
 router.get('/', companiesCtrl.index)
 router.get('/:id', companiesCtrl.show)
-router.post('/', companiesCtrl.create)
+router.post('/', checkCompanyAuth, companiesCtrl.create)
 
 /*---------- Protected Routes ----------*/
 
