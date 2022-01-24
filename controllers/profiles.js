cedad84eed74e35754b4e670c053961abf133a29
UbuntuLex <alexandermaniwang99@gmail.com>
2022-01-24 12:47:31 -0500

    render company page with details in profile page

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,19 +1,27 @@
 import { Profile } from '../models/profile.js'
+import { Company } from '../models/company.js'
 
 function index(req, res) {
   Profile.find({})
-  .then(profiles => res.json(profiles))
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+    .then(profiles => res.json(profiles))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 }
 
-function getProfile(req, res){
+function getProfile(req, res) {
   Profile.findById(req.params.id)
-  .then(profile => {
-    res.json(profile)
-  })
+    .then(profile => {
+      res.json(profile)
+    })
 }
 
-export { index, getProfile }
+function getCompany(req, res) {
+  Company.findOne({ admins: req.params.id })
+    .then(company => {
+      res.json(company)
+    })
+}
+
+export { index, getProfile, getCompany }
